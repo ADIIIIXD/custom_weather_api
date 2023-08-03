49fbcb1968374a10fcaa2a997190635474ed60fd
aditya <addie4120@gmail.com>
2023-08-03 19:47:59 +0530

    displayed stations.txt table on homepage

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import pandas as pd
 
 app = Flask(__name__)
 
+table = pd.read_csv("data_small/stations.txt", skiprows=17)
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=table.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze()/10
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
